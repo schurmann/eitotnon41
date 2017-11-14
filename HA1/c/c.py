1ac5c77f617751a90d7084f0a657cb0242b48d23
schurmann <jonathan.schurmann@gmail.com>
2017-11-14 10:30:52 +0100

    Added customer class

--- HA1/c/c.py
+++ HA1/c/c.py
@@ -5,41 +5,52 @@ from hashlib import sha256
 RANDCEIL = 2048
 N = 123
 K = 1000
-ID = 1
+
+def rand() -> int:
+    return random.randint(0, RANDCEIL) % N
 
 class Bank:
-    bees = []
+    def __init__(self):
+        self.bees = []
 
     def verify_quads(self, chosen_quads: list) -> bool:
         pass
 
     def choose_and_remember(self, bs: list) -> list:
-        bees = bs
+        self.bees = bs
         return random.sample(range(0, 2*K), K)
 
+class Customer:
+    def __init__(self):
+        self.ID = 1
+        self.quads = []
+        self.bees = []
 
-def f(a: bytes, b: bytes) -> int:
-    return int.from_bytes((a + b), byteorder='big')
+    def __f(self, a: bytes, b: bytes) -> int:
+        return int.from_bytes((a + b), byteorder='big')
 
-def rand() -> int:
-    return random.randint(0, RANDCEIL) % N
+    def gen_quads(self, k: int) -> list:
+        self.quads = [(rand(), rand(), rand(), rand()) for i in range(2*k)]
 
-def gen_quads(k: int) -> list:
-    return [(rand(), rand(), rand(), rand()) for i in range(2*k)]
+    def __make_x(self, a: int, c: int) -> bytes:
+        return sha256(a.to_bytes(2, byteorder='big') + c.to_bytes(2, byteorder='big')).digest()
 
-def make_x(a: int, c: int) -> bytes:
-    return sha256(a.to_bytes(2, byteorder='big') + c.to_bytes(2, byteorder='big')).digest()
+    def __make_y(self, a: int, d: int, identifier: int) -> bytes:
+        return sha256((a ^ identifier).to_bytes(2, byteorder='big') + d.to_bytes(2, byteorder='big')).digest()
 
-def make_y(a: int, d: int, identifier: int) -> bytes:
-    return sha256((a ^ identifier).to_bytes(2, byteorder='big') + d.to_bytes(2, byteorder='big')).digest()
+    def __make_b():
+        return (math.pow(r, 3) * f_out) % n
 
-def make_b(r: int, f_out: int, n: int) -> int:
-    return (math.pow(r, 3) * f_out) % n
+
+    def make_bees(self, r: int, f_out: int, n: int) -> int:
+        self.bees = [self__make_b(math.pow(r, 3), self.__f(self.__make_x(a, c), self.__make_y(a, d, ID)), N) for (a, c, d, r) in self.quads]
 
 if __name__ == '__main__':
     bank = Bank()
-    quads = gen_quads(K)
-    bs = [make_b(math.pow(r, 3), f(make_x(a, c), make_y(a, d, ID)), N) for (a, c, d, r) in quads]
-    indexes = bank.choose_and_remember(bs)
-    chosen_quads = [quads[index] for index in indexes]
-    bank.verify_quads(chosen_quads)
+    customer = Customer()
+    customer.gen_quads(K)
+    print(customer.quads)
+    #bs = 
+    #indexes = bank.choose_and_remember(bs)
+    #chosen_quads = [quads[index] for index in indexes]
+    #bank.verify_quads(chosen_quads)
