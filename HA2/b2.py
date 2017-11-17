7884a62543e000c114b21daa2ef3efdc542b46e7
Isak Lindhé <isak.e.lindhe@gmail.com>
2017-11-17 13:01:59 +0100

    as of thursday

--- HA2/b2.py
+++ HA2/b2.py
@@ -2,8 +2,6 @@ from pcapfile import savefile
 from pprint import pprint
 
 def next_batch(index, mix_ip, packets):
-    print('next_batch index = ', index)
-    print('len(packets):',len(packets))
     while index < len(packets) and packets[index].packet.payload.src.decode('UTF8') != mix_ip:
         index += 1
     batch = []
@@ -22,18 +20,31 @@ def find_partners(target_ip:str, mix_ip:str, m:int, filename:str):
     ]
     batches = [next_batch(i, mix_ip, list(cap.packets)) for i in send_indices]
     batchips = [ set(map(lambda x: x.packet.payload.dst.decode('UTF8'), batch)) for batch in batches ]
-    for i in range(0, len(batchips)):
-        for j in range(i + 1, len(batchips)):
-            if len(batchips[i].intersect(batchips[j])) = 0:
-                #save i
-                pass
+    disjunct_ips = get_disjunct_sets(batchips) #indexes for disjoint sets
+    return [batchips[i] for i in disjunct_ips]
+
+
+def disjoint(b1:set, b2:set) -> bool:
+    b =  len(b1.intersection(b2)) == 0
+    return b
 
 
 def get_disjunct_sets(batchips: list):
-    pass
+    disjunct_ips = []
+    for i in range(0, len(batchips)):
+        for j in range(i + 1, len(batchips)):
+            if disjoint(batchips[i], batchips[j]):
+               break
+        disjunct_ips.append(i)
+    return disjunct_ips
+
+def exclude_phase(r: set, others: list):
+    return r.intersection(set(others[0])).intersection(set(others[1]))
         
 
-    pprint(batchips)
-
 if __name__ == '__main__':
-    find_partners('159.237.13.37', '94.147.150.188', 2, 'cia.log.1337.pcap')
+    ip_sets = find_partners('159.237.13.37', '94.147.150.188', 2, 'cia.log.1337.pcap')
+    ip_sets = ip_sets[:2] if len(ip_sets) > 2 else ip_sets
+    r = ip_sets[-1]
+    ans = exclude_phase(r, ip_sets)
+    print(ans)
