7b4a11b62b3b1a8a8b9cb7f5a729cb3fe82d41f4
schurmann <jonathan.schurmann@gmail.com>
2017-12-05 18:51:17 +0100

    labels and legend for plot

--- HA3/plot.py
+++ HA3/plot.py
@@ -1,10 +1,10 @@
 from matplotlib import pyplot as plt
 import sys
 
-def plot(data):
+def plot(data, name):
     xs = [d[0] for d in data]
     ys = [d[1] for d in data]
-    plt.plot(xs, ys) 
+    plt.plot(xs, ys, label=name) 
 
 if __name__ == '__main__':
     plots = sys.argv[1:-1]
@@ -12,6 +12,12 @@ if __name__ == '__main__':
     for i, name in enumerate(plots):
         with open(name, 'r') as f:
             lines = [line.strip().split(':') for line in f]
-            data = list(map(lambda l: (float(l[0]), float(l[1])), lines))
-            plot(data)
-    plt.savefig(f'{pdf_name}.pdf')
+            data = list(map(lambda l: (float(l[0]), 1-float(l[1])), lines))[15:30]
+            plot(data, name)
+    plt.xlabel('X size')
+    plt.ylabel('Breaking probability')
+    plt.annotate('Meeting point', xy=(18, 0.5), xytext=(20, 0.51),
+            arrowprops=dict(facecolor='black', shrink=0.05),
+            )
+    plt.legend()
+    plt.savefig(f'{pdf_name}.png')
