4e351c2b6c0ad56528c46989a763e5c65fb65cfe
schurmann <jonathan.schurmann@gmail.com>
2017-12-12 10:44:24 +0100

    Converting hex to bytes correcly; Test quiz passed

--- HA4/b3.py
+++ HA4/b3.py
@@ -91,6 +91,6 @@ def test_OAEP_decode():
     assert(OAEP_decode(EM) == M)
 
 ###Test quiz###
-print(mgf1(b'9b4bdfb2c796f1c16d0c0772a5848b67457e87891dbc8214', 21).hex()) #Failed
-print(OAEP_encode('c107782954829b34dc531c14b40e9ea482578f988b719497aa0687', '1e652ec152d0bfcd65190ffc604c0933d0423381')) #Failed
+print(mgf1(bytes.fromhex('9b4bdfb2c796f1c16d0c0772a5848b67457e87891dbc8214'), 21).hex()) #Passed
+print(OAEP_encode('c107782954829b34dc531c14b40e9ea482578f988b719497aa0687', '1e652ec152d0bfcd65190ffc604c0933d0423381')) #Passed
 print(OAEP_decode('0063b462be5e84d382c86eb6725f70e59cd12c0060f9d3778a18b7aa067f90b2178406fa1e1bf77f03f86629dd5607d11b9961707736c2d16e7c668b367890bc6ef1745396404ba7832b1cdfb0388ef601947fc0aff1fd2dcd279dabde9b10bfc51efc06d40d25f96bd0f4c5d88f32c7d33dbc20f8a528b77f0c16a7b4dcdd8f')) #Passed
